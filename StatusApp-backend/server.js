5c506fbd76408b43c8503d58ecad6f938cbe8764
suranjan2003 <karmakarsuranjan12@gmail.com>
2025-04-10 20:16:59 +0530

    single server deployment non-working code

--- StatusApp-backend/server.js
+++ StatusApp-backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import serviceRoutes from './routes/service.route.js';
 import cors from 'cors';
+import path from 'path';
 dotenv.config();
 
 
@@ -11,11 +12,18 @@ const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve(); // get the current directory name
+
 app.use(express.json()); // allows us to access json data in the req.body
 
 app.use('/api/services', serviceRoutes); // use the service routes
 
-
+if(process.env.NODE_ENV === 'production'){
+	app.use(express.static(path.join(__dirname, '/StatusApp/dist'))); // serve the static files from the client/build directory
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, "StatusApp", "dist", "index.html"));
+	});
+}
 
 
 
